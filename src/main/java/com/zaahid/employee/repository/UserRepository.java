2492763ccde0ae6b6af78f0fe333e975e4d94083
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-15 04:52:32 +0700

    configure user repository

--- src/main/java/com/zaahid/employee/repository/UserRepository.java
+++ src/main/java/com/zaahid/employee/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package com.zaahid.employee.repository;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.zaahid.employee.model.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	@Query("SELECT u from User u Where u.username = :username")
+	public User getUserByUsername(@Param("username") String username);
+	
 }
