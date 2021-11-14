35bb1181829c12dd251c21e38d9e5563e5cd55f1
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-15 04:08:07 +0700

    extend to jpa repository

--- src/main/java/com/zaahid/employee/repository/EmployeeRepository.java
+++ src/main/java/com/zaahid/employee/repository/EmployeeRepository.java
@@ -1,5 +1,11 @@
 package com.zaahid.employee.repository;
 
-public class EmployeeRepository {
+import com.zaahid.employee.model.Employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     
 }
