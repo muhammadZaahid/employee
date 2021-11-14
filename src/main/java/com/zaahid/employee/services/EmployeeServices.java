dec2306803c5c329ba028b03697a4b96ea1ccf88
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-15 04:11:38 +0700

    add method crud in service

--- src/main/java/com/zaahid/employee/services/EmployeeServices.java
+++ src/main/java/com/zaahid/employee/services/EmployeeServices.java
@@ -1,5 +1,36 @@
 package com.zaahid.employee.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zaahid.employee.model.Employee;
+import com.zaahid.employee.repository.EmployeeRepository;
+
+@Service
 public class EmployeeServices {
-    
+
+	@Autowired
+	private EmployeeRepository repo;
+	
+	public List<Employee> listAll(){
+		return repo.findAll();
+	}
+	
+	public void save(Employee employee) {
+		repo.save(employee);
+	}
+	
+	public Employee get(Long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+	
+	
 }
+
+
